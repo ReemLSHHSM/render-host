a7fdfc81c0c381572a157d99b90783285ad0de01
ReemLSHHSM <reemlhm72@gmail.com>
2024-05-08 13:22:27 +0000

    fixing pg_hosting

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //require pg
-//const {Client}=require('pg');
+const pg=require('pg');
 
 //url of database i want to connect with
 const url = process.env.URL;
